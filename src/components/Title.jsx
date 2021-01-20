902b5ebe8d9f59415907c20c026fe4ad145c00dd
Kamronbek <kamronbek028@gmail.com>
2021-01-20 21:20:15 +0500

    added title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = ({ title }) => {
+  return (
+    <div>
+      <h1 className="heading-secondary">{title}</h1>
+    </div>
+  );
+};
+
+export default Title;
