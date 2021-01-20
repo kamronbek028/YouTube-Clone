65cb049fad1d77f652f7f0b42b5f401c5965cb71
Kamronbek <kamronbek028@gmail.com>
2021-01-20 21:06:02 +0500

    added error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import errorImage from "../assets/404.svg";
 
 const Error = () => {
-  return <div>error</div>;
+  return (
+    <div className="error">
+      <img className="error__img" src={errorImage} alt="404" />
+      <h1 className="heading-primary">Ooops, there is no page here...</h1>
+      <div className="button">
+        <Link to="/">
+          <button className="btn btn--error">Go home</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
