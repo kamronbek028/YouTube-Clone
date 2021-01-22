df407c769359568e7af071908f0e7aa30d195b70
Kamronbek <kamronbek028@gmail.com>
2021-01-22 19:40:00 +0500

    added properties into watch-video component

--- src/components/WatchVideo.jsx
+++ src/components/WatchVideo.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { FaHeart, FaHeartBroken, FaShare, FaDownload } from "react-icons/fa";
+
+import FormatData from "../functions/FormatData";
+import FormatLocation from "../functions/FormatLocation";
+import FormatNumber from "../functions/FormatNumber";
+import LimitTitle from "../functions/LimitTitle";
 
 const WatchVideo = ({ video, channel }) => {
   const { id } = video;
@@ -11,18 +19,69 @@ const WatchVideo = ({ video, channel }) => {
   } = video.snippet;
   const { viewCount, likeCount, dislikeCount } = video.statistics;
 
-  const { medium } = channel.snippet.thumbnails;
+  const { url } = channel.snippet.thumbnails.medium;
   const { subscriberCount } = channel.statistics;
 
   return (
-    <iframe
-      className="about-video"
-      src={`https://www.youtube.com/embed/${id}?autoplay=1`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      autoPlay
-    ></iframe>
+    <div className="watch">
+      <iframe
+        className="about-video"
+        src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        autoPlay
+      />
+
+      <div className="info">
+        <div className="info-top">
+          <div className="video-text">
+            <p className="video-title u-margin-bottom-extra-small">{title}</p>
+            <p className="video-secondary">
+              {FormatNumber(viewCount)} views &#8226; {FormatData(publishedAt)}
+            </p>
+          </div>
+          <div className="video-like">
+            <Link to="">
+              <FaHeart className="like__icon" />
+              <p>{FormatNumber(likeCount)}</p>
+            </Link>
+
+            <Link to="">
+              <FaHeartBroken className="like__icon" />
+              <p>{FormatNumber(dislikeCount)}</p>
+            </Link>
+
+            <Link to="">
+              <FaShare className="like__icon" />
+              <p>SHARE</p>
+            </Link>
+
+            <Link to="">
+              <FaDownload className="like__icon" />
+              <p>DOWNLOAD</p>
+            </Link>
+          </div>
+        </div>
+        <div className="info-bottom">
+          <div className="info-bottom__info">
+            <a href={`https://www.youtube.com/channel/${channelId}`}>
+              <img src={url} alt="channel" className="video__info__img" />
+            </a>
+            <div className="search-info">
+              <a href={`https://www.youtube.com/channel/${channelId}`}>
+                <p className="video-title">{channelTitle}</p>
+                <p className="video-secondary u-margin-bottom-small subscriberCount">
+                  {FormatNumber(subscriberCount)} subscribers
+                </p>
+              </a>
+            </div>
+
+            <p className="video-description">{description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
