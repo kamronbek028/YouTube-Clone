6b95b6986942952ec25dde5faa23be585298987e
Kamronbek <kamronbek028@gmail.com>
2021-01-22 13:05:04 +0500

    updated url of videos

--- src/components/PopularVideo.jsx
+++ src/components/PopularVideo.jsx
@@ -18,7 +18,7 @@ const PopularVideo = ({ videos }) => {
           const { viewCount } = video.statistics;
 
           return (
-            <Link to={`/videoID:${id}`} key={index}>
+            <Link to={`/watch?v=${id}`} key={index}>
               <div className="video">
                 <img src={url} alt={title} className="video__img" />
                 <div className="video__info">
